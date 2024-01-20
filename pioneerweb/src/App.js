c1f209e60662f03cc47d439131baa1d5d725d4c1
mileslow <40550490+mileslow@users.noreply.github.com>
2024-01-20 14:49:48 -0500

    push initial

---
+++ pioneerweb/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="wrapper">
+      <h1>
+        Pioneer Software<span className="dot">.</span>
+      </h1>
+      <p>Where your business meets the future</p>
+      <div style={{ display: "flex" }}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
